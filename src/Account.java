11e26eac2a2d3f36415a3524d50820e5984eb51b
htnha <hoang.tuan.nha@gmail.com>
2022-09-08 09:03:05 +0700

    Thêm hàm deposit va withdraw

--- src/Account.java
+++ src/Account.java
@@ -19,10 +19,25 @@ public class Account {
     void display(){
         System.out.println("STK:" + id +",  Ten TK:" + name + ", So du:"+ balance);
     }
+    void deposit(int amount){
+        this.balance += amount;
+        System.out.println("So du trong tai khoan hien tai la: " + this.balance);
+    }
+    void withdraw(int amount){
+        if(amount <= this.balance){
+            this.balance -= amount;
+            System.out.println("So du con lai trong tai khoan: " + this.balance);
+        }else{
+            System.out.println("So du trong tai khoan khong du");
+        }
+    }
     public static void main(String[]args){
         Account tk1 = new Account(92,"Hong Tam");
         Account tk2 = new Account(232, "Sinh Duc", 1000000);
         tk1.display();
         tk2.display();
+        tk1.deposit(500000);
+        tk2.withdraw(2000000);
+        tk1.withdraw(100000);
     }
 }
